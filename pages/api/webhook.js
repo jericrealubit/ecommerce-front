7668dcdb129942a51e7632fdd2053eebae3feda4
jeric realubit <jericrealubit@gmail.com>
2023-09-04 21:50:11 +1200

    payment webhook

---
+++ pages/api/webhook.js
@@ -0,0 +1,47 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { buffer } from "micro";
+
+const endpointSecret =
+  "whsec_0f0ff48c5ab149610e77f45baa5e699d74ba51351bc777dc8c6aae3571e3dacc";
+
+const handler = async (req, res) => {
+  await mongooseConnect();
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await buffer(req),
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        Order.findByIdAndUpdate(orderId, { paid: true });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send("ok");
+};
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+export default handler;
