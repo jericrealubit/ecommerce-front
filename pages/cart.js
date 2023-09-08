ff00062e1f56616a092a38505dfce27d63bc4b58
jeric realubit <jericrealubit@gmail.com>
2023-09-08 21:11:08 +1200

    done mobile

--- pages/cart.js
+++ pages/cart.js
@@ -13,7 +13,10 @@ import { useRouter } from "next/router";
 
 const ColumnsWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1.3fr 0.7fr;
+  grid-template-columns: 1;
+  @media screen and (min-width: 768px) {
+    grid-template-columns: 1.3fr 0.7fr;
+  }
   gap: 20px;
   margin-top: 40px;
 `;
@@ -29,17 +32,26 @@ const ProductInfoCell = styled.div`
 `;
 
 const ProductImageBox = styled.div`
-  width: 100px;
+  width: 70px;
   height: 100px;
-  padding: 10px;
+  padding: 2px;
   border: 1px solid rgba(0, 0, 0, 0.1);
   display: flex;
   align-items: center;
   justify-content: center;
   border-radius: 10px;
   img {
-    max-width: 80px;
-    max-height: 80px;
+    max-width: 40px;
+    max-height: 40px;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 10px;
+    width: 100px;
+    height: 100px;
+    img {
+      max-width: 60px;
+      max-height: 60px;
+    }
   }
 `;
 
@@ -48,7 +60,12 @@ const ProductTitle = styled.div`
 `;
 
 const QuantityLabel = styled.span`
-  padding: 0 3px;
+  padding: 0 15px;
+  display: block;
+  @media screen and (min-width: 768px) {
+    padding: 0 4px;
+    display: inline-block;
+  }
 `;
 
 const CityHolder = styled.div`
