4f212aef7ae82bc6e715197cd49164c9dbc4645c
jeric realubit <jericrealubit@gmail.com>
2023-09-22 12:52:06 +1200

    remove inactive links

--- components/Header.js
+++ components/Header.js
@@ -14,11 +14,7 @@ const Logo = styled(Link)`
   position: relative;
   z-index: 3;
 `;
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 20px 0;
-`;
+
 const StyledNav = styled.nav`
   ${(props) =>
     props.mobileNavActive ? ` display: block; ` : ` display: none; `}
@@ -46,6 +42,12 @@ const NavLink = styled(Link)`
   }
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+`;
+
 const NavButton = styled.button`
   background-color: transparent;
   width: 30px;
@@ -71,8 +73,8 @@ const Header = () => {
           <StyledNav mobileNavActive={mobileNavActive}>
             <NavLink href={"/"}>Home</NavLink>
             <NavLink href={"/products"}>All products</NavLink>
-            <NavLink href={"/categories"}>Categories</NavLink>
-            <NavLink href={"/account"}>Account</NavLink>
+            {/* <NavLink href={"/categories"}>Categories</NavLink>
+            <NavLink href={"/account"}>Account</NavLink> */}
             <NavLink href={"/cart"}>Cart ({cartProducts.length})</NavLink>
           </StyledNav>
           <NavButton onClick={() => setMobileNavActive((prev) => !prev)}>
